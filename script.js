d196081c4dad1d0cee0247232a34898b0b5703a6
adriankoder <adrian.koder@gmail.com>
2024-06-26 11:05:36 +0200

    test

---
+++ script.js
@@ -0,0 +1,16 @@
+const myarray = [
+    {movie: "finding nemo"},
+    {movie: "the lion king"},
+    {movie: "the little mermaid"},
+];
+//wiew the whole array
+console.log(myarray);
+
+//wiew a specific object
+console.log(myarray[0]);
+
+//view the data
+console.log(myarray.movie);
+
+//wiew a specific data
+console.log(myarray[0].movie);
